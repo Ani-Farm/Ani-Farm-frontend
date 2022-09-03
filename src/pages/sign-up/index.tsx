78a1135d731f087bff1eaa3727a8efba7b80647e
seunghwan <sun1238799@naver.com>
2022-09-03 16:10:59 +0900

    회원가입 페이지퍼블리싱

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -2,76 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Navbar from "../../components/navbar";
+import { Container } from "../../components/navbar/styles";
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as S from "./styles";
 
-export const Container = styled.div`
-  height: 100%;
-  width: 100%;
-
-  display: flex;
-  justify-content: center;
-`;
-
-export const SContainer = styled.div`
-  height: 675px;
-  width: 656px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export const LoginName = styled.div`
-  font-size: 36px;
-  line-height: 52px;
-  font-weight: 700;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  font-family: "Noto Sans KR";
-  font-style: normal;
-  margin-top: 100px;
-  margin-bottom: 36px;
-  margin-left: 76px;
-  color: #000000;
-`;
-
-export const Input = styled.input`
-  width: 576px;
-  height: 84px;
-
-  background: #fafafa;
-  box-shadow: inset 1px 1px 2px rgba(0, 0, 0, 0.25);
-  border-radius: 5px;
-  margin-bottom: 23px;
-  border-radius: 5px;
-`;
-
-const Line = styled.hr`
-  color: rgba(0, 0, 0, 0.65);
-  width: 100%;
-`;
+interface ISignUp {
+  Nick: String;
+  id: String;
+  password: String;
+  passwordCheck: String;
+}
 
 const SignUpPage = () => {
+  const { register, handleSubmit } = useForm<ISignUp>();
+  const onSubmit: SubmitHandler<ISignUp> = (data) => console.log(data);
   return (
     <>
       <Navbar />
-      <Container style={{ marginTop: "60px" }}>
-        <SContainer>
-          <LoginName>로그인</LoginName>
-          <Input></Input>
-          <Input></Input>
-          <button>눌러</button>
-          <Line />
-          <button>눌러</button>
-          <div>
-            계정이 없나요?
-            <span>
-              <Link to="#">가입</Link>
-            </span>
-          </div>
-        </SContainer>
-      </Container>
+      <S.SignBox>
+        <S.Title>회원 가입</S.Title>
+        <S.SignForm onSubmit={handleSubmit(onSubmit)}>
+          <label>닉네임</label>
+          <S.Input placeholder="닉네임을 입력하세요" {...register("Nick")} />
+          <label>아이디</label>
+          <S.Input placeholder="아이디를 입력하세요" {...register("id")} />
+          <label>비밀번호</label>
+          <S.Input
+            placeholder="비밀번호를 입력하세요"
+            {...register("password")}
+          />
+          <label>비밀번호 확인</label>
+          <S.Input
+            placeholder="비밀번호를 입력하세요"
+            {...register("passwordCheck")}
+          />
+          <S.Button type="submit">가입하기</S.Button>
+        </S.SignForm>
+      </S.SignBox>
     </>
   );
 };
--- src/pages/sign-up/styles.tsx
+++ src/pages/sign-up/styles.tsx
@@ -1,1 +1,83 @@
 import styled from "styled-components";
+
+export const SignBox = styled.div`
+  width: 70%;
+  margin: 60px auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 32px;
+  margin: 2rem;
+
+  @media screen and (min-width: 768px) {
+    font-size: 40px;
+  }
+  @media screen and (min-width: 1024px) {
+    font-size: 48px;
+  }
+`;
+
+export const SignForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+export const Input = styled.input`
+  width: 373px;
+  height: 42px;
+
+  margin-top: 10px;
+  margin-bottom: 50px;
+  background: #fafafa;
+  box-shadow: inset 1px 1px 1px rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+
+  @media screen and (min-width: 768px) {
+    height: 50px;
+    width: 403px;
+  }
+  @media screen and (min-width: 1024px) {
+    height: 58px;
+    width: 433px;
+  }
+`;
+
+export const Button = styled.button`
+  width: 300px;
+  height: 50px;
+
+  margin: 0 auto;
+  border: none;
+  background: #d9d9d9;
+  border-radius: 10px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #000000;
+
+  :hover {
+    font-size: 22px;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 21px;
+    :hover {
+      font-size: 23px;
+    }
+  }
+  @media screen and (min-width: 1024px) {
+    font-size: 23px;
+    :hover {
+      font-size: 25px;
+    }
+  }
+`;
